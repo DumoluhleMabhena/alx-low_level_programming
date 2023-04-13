b6ddd0098b1f93ed9a5e6a08d08e98a0f0df610e
DumoluhleMabhena <dumo.95m@gmail.com>
2023-04-13 10:01:39 -0700

    first commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * malloc_checked - function that allocates memory using malloc
+ * @b: number of bytes to allocate
+ * Return: a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *hdlr;
+
+	hldr = malloc(b);
+
+	if (hldr == NULL)
+		exit(98);
+
+	return (hldr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This folder contains all the files needed for the 0x0C-more_malloc_free task.
