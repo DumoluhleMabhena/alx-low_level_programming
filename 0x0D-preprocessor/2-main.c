64796c20ffec5bef2f0f1cff6960c9f455c1515b
DumoluhleMabhena <dumo.95m@gmail.com>
2023-04-17 16:29:46 -0700

    third commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,2 +1,10 @@
-#include <stdlib.h>
 #include <stdio.h>
+/**
+ * main - prints the current file path name
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	printf("%s\n",_FILE_);
+	return (0);
+}
