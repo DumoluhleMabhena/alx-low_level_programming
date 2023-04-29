6c171fb75af9e330949a7cf1bdc03dbfa7712681
DumoluhleMabhena <dumo.95m@gmail.com>
2023-04-28 17:27:45 -0700

    first commit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,19 @@
-#ifndef LINKED_LISTS
-#define LINKED_LISTS
+#ifndef LINKED_LISTS_
+#define LINKED_LISTS_
+
+/**
+ * struct list_s - singly linked list as required by the task
+ * @str: string
+ * @len: size of string
+ * @next: pointer to the next node in the list.
+ * Description: singly linked list node struct
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
@@ -8,3 +22,4 @@ list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
 #endif
+
