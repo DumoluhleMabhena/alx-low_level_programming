97cafbab82936c46283fe2ea48ec24bc52b84d68
Dumolule Mabhena <dumo.95m@gmail.com>
2023-08-14 16:38:25 -0700

    task three

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the file that the user wants
+ *
+ * Return: Always 0 (Success) for success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
