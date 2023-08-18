19b2298bbec9d80fc0b76f994f6178ab1c260b7b
Dumolule Mabhena <dumo.95m@gmail.com>
2023-08-17 20:31:31 -0700

    second task

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_numbers - Prints numbnew line.
+ * @separator: The se printed between numbers.
+ * @n: The n integers passed to the function.
+ * @...: A variablof numbers to be printed.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list numszs;
+	unsigned int ix;
+
+	va_start(numszs, n);
+
+	for (ix = 0; ix < n; ix++)
+	{
+		printf("%d", va_arg(numzss, int));
+
+		if (ix != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+
+	va_end(numszs);
+}
