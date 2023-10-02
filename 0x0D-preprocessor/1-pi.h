792f769982b133ebf98be0ac3b88c92e8bd524bb
DumoluhleMabhena <89202217+DumoluhleMabhena@users.noreply.github.com>
2023-10-02 15:34:29 +0200

    Delete 0x0D-preprocessor directory

--- 0x0D-preprocessor/1-pi.h
+++
@@ -1,6 +0,0 @@
-#ifndef OBJECT_LIKE_PI
-#define OBJECT_LIKE_PI
-
-#define PI 3.14159265359
-
-#endif
--- 0x0D-preprocessor/4-sum.h
+++
@@ -1,6 +0,0 @@
-#ifndef SUM_OBJECT_H
-#define SUM_OBJECT_H
-
-#define SUM(x, y) ((y) + (x))
-
-#endif
